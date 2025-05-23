4a11715c802124d91b44ff7b2ee15aac1fb3e668
KaueSC02 <ra142797@uem.br>
2025-05-23 14:45:57 -0300

    Treinando o modelo

--- Object_Recognition/main.py
+++ Object_Recognition/main.py
@@ -4,16 +4,14 @@ import cv2
 import numpy as np
 import torch
 
-# Configura o modelo YOLOv5
 try:
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
     model.to('cuda' if torch.cuda.is_available() else 'cpu').eval()
-    print("Modelo YOLOv5 carregado com sucesso!")
 except Exception as e:
-    print(f"Erro ao carregar o modelo: {e}")
+    print(e)
     exit()
 
-URI = "ws://192.168.3.73:81"
+URI = "ws://192.168.0.111:81"
 WINDOW_NAME = "Detecção de Objetos"
 
 
@@ -26,29 +24,24 @@ async def receive_frames():
                 try:
                     data = await websocket.recv()
 
-                    # Converte os bytes para imagem
+
                     np_data = np.frombuffer(data, dtype=np.uint8)
                     frame = cv2.imdecode(np_data, cv2.IMREAD_COLOR)
 
                     if frame is None:
-                        print("⚠️ Frame inválido recebido.")
                         continue
 
-                    # Realiza inferência
-                    with torch.no_grad():  # Desativa gradientes para inferência
+                    with torch.no_grad():
                         results = model(frame)
 
-                    # Renderiza as detecções
                     results.render()
                     if len(results.ims) > 0:
                         annotated = results.ims[0]
                         cv2.imshow(WINDOW_NAME, annotated)
 
-                    # Verifica se a janela ainda existe
                     if cv2.getWindowProperty(WINDOW_NAME, cv2.WND_PROP_VISIBLE) < 1:
                         break
 
-                    # Delay para exibição e verificação de tecla 'q'
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
 
